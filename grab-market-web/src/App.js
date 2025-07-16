15049429d8546c641dc2bc5f1a2cb0f255bae368
JIN-MIRAE <oggi@seongji.co.kr>
2025-07-16 16:56:25 +0900

    Installed react-router-dom & lecture 62

--- grab-market-web/src/App.js
+++ grab-market-web/src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
-import MainPage from "./main/index.js"
+import MainPage from "./main/index.js";
+import {Routes, Route} from 'react-router-dom';
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 
 function App() {
 
   return (
-    <MainPage></MainPage>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>}>
+      
+        </Route>
+        <Route path="/product/:id" element={<ProductPage></ProductPage>}>
+      
+        </Route>
+        
+        <Route path="/upload" element={<UploadPage></UploadPage>}>
+
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
