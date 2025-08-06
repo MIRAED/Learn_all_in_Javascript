30e6d22091556211e43d02f84fb81e60cc5925a2
JIN-MIRAE <oggi@seongji.co.kr>
2025-08-06 11:28:07 +0900

    Used process.env.NODE_ENV

--- grab-market-web/src/config/constants.js
+++ grab-market-web/src/config/constants.js
@@ -1,2 +1,8 @@
-export const API_URL = 'http://localhost:8080';
+const BACKEND_URL_PROD = 'http://ec2-54-145-218-142.compute-1.amazonaws.com:8080';
+const BACKEND_URL_DEV = 'http://localhost:8080';
+
+export const API_URL = 
+    process.env.NODE_ENV === "production"
+        ? BACKEND_URL_PROD
+        : BACKEND_URL_DEV;
 
